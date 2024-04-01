8f00db1da006536cb96cbd1c27a396bc36895750
Alexander Wolfe <LogicCreates123@Gmail.com>
2024-04-01 15:14:01 -0400

    determing next step. the middle is going to be the most complex because it will be similar to an excel or google sheet

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
+//getting the root of the html file
 const body = document.querySelector('body')
 //returning the html from components
+
 let html_Header = makeHeader();
 let form = makeForm();
 let newMainForChoices = makeMainChoices();
 let pricingSection = PricingArea();
+
 //appending the html components to the root (body in this case)
 body.appendChild(html_Header);
 body.appendChild(form);
@@ -11,17 +14,37 @@ body.appendChild(pricingSection)
 body.appendChild(newMainForChoices);
 
 
-
 let checkBoxContainer = makeReusableGenericDiv('div-checkbox')
+let totalPrice = {total:0};
+let checkBoxInteriorPainting = makeCheckBox('extra-work', "Interior Painting: ", 0, 300, 'checkbox-interior-p', totalPrice);
+let checkBoxWallBottoms = makeCheckBox('extra-work', "Wall Bottoms: ", 1, 150, 'checkbox-wall-b', totalPrice);
+let checkBoxToeKick = makeCheckBox('extra-work', "Toe-Kick: ", 2, 50, 'checkbox-toekick', totalPrice);
 
-let totalPrice = {total:0}
-
-let checkBoxInteriorPainting = makeCheckBox('extra-work', "Interior Painting: ", 0, 300, 'checkbox-interior-p', totalPrice)
-let checkBoxWallBottoms = makeCheckBox('extra-work', "Wall Bottoms: ", 1, 150, 'checkbox-wall-b', totalPrice)
-let checkBoxToeKick = makeCheckBox('extra-work', "Toe-Kick: ", 2, 50, 'checkbox-toekick', totalPrice)
 newMainForChoices.appendChild(checkBoxInteriorPainting);
 newMainForChoices.appendChild(checkBoxWallBottoms);
 newMainForChoices.appendChild(checkBoxToeKick);
 
 body.appendChild(checkBoxContainer);
 let pricingTable = document.querySelector('.pricing-table')
+
+
+
+//          need to make a section that acts a google sheet that has rows inserted.
+//                      Or rather a table?   
+
+//                                           App Layout
+//-----------------------------------
+// inputs for specific work order information
+//-----------------------------------------------------------------------
+// <label> </label> <input> </input> | <label> </label> <input> </input>| 
+
+//Submit button     |          <button>Submit</button                   |
+//                  ---------------------------------------------------- 
+//  Type_of_cabinet | widths | heights | opening width | opening height |
+//                  |   []   |    []   |      []       |       []       | cabinet object = (width, height, opening width, opening height)
+//                  ====================================================
+//Should be how many pieces and pieces would be what determines the amount of rows. 
+//Add an add single row for mistakes or a remove row to remove a row from the rows.
+// Cabinet Object -> Cabinet_object_array -> calulatePrice() -> return price total after doing the math for the square footage of the cabinets.
+
+
