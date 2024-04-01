6d98c8331f5c61769d9ddbface8730953a8d7d03
Alexander Wolfe <LogicCreates123@Gmail.com>
2024-04-01 00:33:27 -0400

    added header component

---
+++ components/Header.js
@@ -0,0 +1,8 @@
+
+function makeHeader(){
+let header = document.createElement('header');
+let h1_header = document.createElement('h1');
+header.appendChild(h1_header)
+h1_header.innerText = 'Cabinet Estimation'
+    return header;
+}
---
+++ main.js
@@ -0,0 +1,6 @@
+
+const body = document.querySelector('body')
+
+let html_Header = makeHeader();
+
+body.appendChild(html_Header)
