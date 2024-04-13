1b91efac71a06382c9593c6b64ddddadb2e19377
Alexander Wolfe <LogicCreates123@Gmail.com>
2024-04-13 02:47:49 -0400

    finished event listeners for gathering information for the location object

--- orderingTool/client/app.js
+++ orderingTool/client/app.js
@@ -77,7 +77,23 @@
 
 let accountSubmissionObj = {
     cabinetType : null,
-    cabinetColor: null
+    cabinetColor: null,
+    hingeOverlay: null,
+    pullType: null,
+    pullSize:null,
+    locationName: null
+}
+
+const createAccount = (submissionObject)=>{ //this is to finalize the location
+const accountSubmissionObj = {
+    cabinetType : submissionObject.cabinetType,
+    cabinetColor: submissionObject.cabinetColor,
+    hingeOverlay: submissionObject.hingeOverlay,
+    pullType: submissionObject.pullType,
+    pullSize:submissionObject.pullSize,
+    locationName:submissionObject.locationName,
+}
+return accountSubmissionObj;
 }
 
 
@@ -95,23 +111,54 @@ document.querySelectorAll('.div-choice-cabinet')[2].addEventListener('click', ()
 //end of style
 // Violet Teal, Crimson Black Silver, Burnt Orange, Green Orange Pink, Bright Aqua
 //buttons for choosing the color of cabinets
-document.querySelectorAll('.div-choice-color')[0].addEventListener('click', ()=>{
-    accountSubmissionObj.cabinetColor = document.querySelectorAll('.div-choice-color p')[0].innerText;
-    });
-    document.querySelectorAll('.div-choice-color')[1].addEventListener('click', ()=>{
-         accountSubmissionObj.cabinetColor = document.querySelectorAll('.div-choice-color p')[1].innerText 
-    });
-    document.querySelectorAll('.div-choice-color')[2].addEventListener('click', ()=>{
- accountSubmissionObj.cabinetColor = document.querySelectorAll('.div-choice-color p')[2].innerText
-    });
-    document.querySelectorAll('.div-choice-color')[3].addEventListener('click', ()=>{
-         accountSubmissionObj.cabinetColor = document.querySelectorAll('.div-choice-color p')[3].innerText
-    });
-    document.querySelectorAll('.div-choice-color')[4].addEventListener('click', ()=>{
-        accountSubmissionObj.cabinetColor = document.querySelectorAll('.div-choice-color p')[4].innerText
-    });
+    document.querySelectorAll('.div-choice-color').forEach((button, i)=>{
+        button.addEventListener('click', ()=>{
+            accountSubmissionObj.cabinetColor = document.querySelectorAll('.div-choice-color p')[i].innerText;
+            console.log(accountSubmissionObj.cabinetColor)
+        })
+    })
 //end of color
 
 //radio buttons for the hinges
-
+// if (document.getElementById('radioID').checked) {
+//     console.log('The radio button is checked');
+// }
 //end of hinges
+
+document.querySelectorAll('.radio-choice-hinge')[0].addEventListener('click', ()=>{
+    accountSubmissionObj.hingeOverlay = .5;
+})
+document.querySelectorAll('.radio-choice-hinge')[1].addEventListener('click', ()=>{
+    accountSubmissionObj.hingeOverlay = 1;
+})
+document.querySelectorAll('.radio-choice-hinge')[2].addEventListener('click', ()=>{
+    accountSubmissionObj.hingeOverlay = 1.25;
+})
+document.querySelectorAll('.radio-choice-hinge')[3].addEventListener('click', ()=>{
+    accountSubmissionObj.hingeOverlay = 1.5;
+})
+//pullType 
+document.querySelectorAll('.button-choice-pull').forEach((button, i)=>{
+    button.addEventListener('click', ()=>{
+        accountSubmissionObj.pullType = document.querySelectorAll('#p-pull-type')[i].innerText;
+        console.log(accountSubmissionObj.pullType);
+    })
+})
+
+//pullSize
+document.querySelectorAll('.div-four-inch')[0].addEventListener('click', ()=>{
+    
+    console.log(accountSubmissionObj.pullSize);
+
+});
+
+//Location name
+document.querySelector('#submit-location-name').addEventListener('click', (e)=>{
+    // e.preventDefault()
+    accountSubmissionObj.locationName = document.querySelector('#input-location-name').value;
+})
+
+
+document.querySelector('.button-choice-save-location').addEventListener('click',()=>[
+    
+])
